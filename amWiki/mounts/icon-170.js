453f72e79802744e576d6a3a4d1024b79d5bb5da
tianyunchong <854024109a>
2021-05-22 10:27:32 +0800

    up

--- amWiki/mounts/icon-170.js
+++ amWiki/mounts/icon-170.js
@@ -1,1 +1,1 @@
-if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['icon']={"name":"icons.svg","path":"../amWiki/images/icons.svg","content":"","timestamp":1621649865856}
+if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['icon']={"name":"icons.svg","path":"../amWiki/images/icons.svg","content":"","timestamp":1621650439199}
---
+++ library/002-学习/002-工具/002-开发工具/001-mac-vim.md
@@ -0,0 +1,7 @@
+```shell
+使用neovim
+http://www.eryajf.net/4733.html
+https://www.cnblogs.com/cniwoq/p/13272746.html
+coc.nvim配置
+https://www.cnblogs.com/hystill/p/13783183.html
+```
