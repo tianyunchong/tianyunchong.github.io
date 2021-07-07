878a1c0d5ee49460ff62a8be162b7dc0eab3aa74
tianyunchong <854024109a>
2021-07-07 11:21:12 +0800

    新增水印图片生成

--- amWiki/mounts/icon-170.js
+++ amWiki/mounts/icon-170.js
@@ -1,1 +1,1 @@
-if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['icon']={"name":"icons.svg","path":"../amWiki/images/icons.svg","content":"","timestamp":1625205005577}
+if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['icon']={"name":"icons.svg","path":"../amWiki/images/icons.svg","content":"","timestamp":1625628056297}
